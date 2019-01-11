e246cbae43a69e33f352e1c1f95f2e8c61cfb699
gtihtina <girumtihtina@hotmail.com>
2019-01-10 19:42:12 -0800

    login button working as expected

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import EventsRecycler from './src/EventsRecycler';
-import EventDetail from './src/EventDetail';
+import EventsRecycler from './src/components/EventsRecycler';
+import EventDetail from './src/components/EventDetail';
+import Login from './src/components/Login';
 import { createStackNavigator, createAppContainer } from "react-navigation";
-import Login from './src/components/Login/Login';
+
 const AppNavigator = createStackNavigator(
   {
     Home: {
+      screen: Login
+    },
+    EventsRecycler:{
       screen: EventsRecycler
     },
     EventDetail: {
@@ -21,19 +25,6 @@ export default class App extends React.Component {
   render() {
     return (
       <AppContainer />
-      // <Login /> THIS WILL REPLACE WITH LOGIN t
     );
   }
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#aaa',
-//     },
-//     appName:{
-//       fontSize: 40,
-//       fontWeight: 'bold',
-//       alignSelf: 'center',
-//     }
-// });
---
+++ src/components/Login.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import {Text, View, TextInput, TouchableOpacity, Alert, Button, StyleSheet, StatusBar, KeyboardAvoidingView} from 'react-native';
+
+export default class Login extends Component {
+    static navigationOptions = {
+        title: 'Our lit app',
+      };
+    render() {
+        return (
+            <View style={styles.viewContainer}>
+                <KeyboardAvoidingView behavior="padding" style={styles.container}>
+                <TextInput style = {styles.input} 
+                autoCapitalize="none" 
+                onSubmitEditing={() => this.passwordInput.focus()} 
+                autoCorrect={false} 
+                keyboardType='email-address' 
+                returnKeyType="next" 
+                placeholder='Email' 
+                placeholderTextColor='rgba(225,225,225,0.7)'/>
+
+                <TextInput style = {styles.input}   
+                returnKeyType="go" 
+                ref={(input)=> this.passwordInput = input} 
+                placeholder='Password' 
+                placeholderTextColor='rgba(225,225,225,0.7)' 
+                secureTextEntry/>
+
+                <TouchableOpacity 
+                style={styles.buttonContainer} 
+                onPress={() => this.props.navigation.navigate("EventsRecycler")}>
+                    <Text  style={styles.buttonText}>LOGIN</Text>
+                </TouchableOpacity> 
+            </KeyboardAvoidingView>
+            </View>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    viewContainer: {
+        flex: 1,
+        backgroundColor: '#2c3e50',
+    },
+    logContainer:{
+        alignItems: 'center',
+        flexGrow: 1,
+        justifyContent: 'center'
+    },
+    logo: {
+        position: 'absolute',
+        width: 300,
+        height: 100
+    },
+    title: {
+        color: '#FFF',
+        marginTop: 10,
+        width: 160,
+        textAlign: 'center',
+        opacity: 0.9
+    },
+    container: {
+        padding: 20
+       },
+       input:{
+           height: 40,
+           backgroundColor: 'rgba(225,225,225,0.2)',
+           marginBottom: 10,
+           padding: 10,
+           color: '#fff'
+       },
+       buttonContainer:{
+           backgroundColor: '#2980b6',
+           paddingVertical: 15
+       },
+       buttonText:{
+           color: '#fff',
+           textAlign: 'center',
+           fontWeight: '700'
+       },
+})
